7ffb6cbadc18789f9cf14a177f91a5b934c600e2
Aytmurat Eshbaev <a.eshbaev1@gmail.com>
2022-09-19 11:46:18 +0500

    save

--- src/index.js
+++ src/index.js
@@ -1,45 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import data from "./data";
-import Quote from "./Quote";
 
-class Quotes extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            index: 0
-        }
-        this.handleClick = this.handleClick.bind(this);
-    }
+function Clock() {
+    const [time, setTime] = useState(new Date().toLocaleTimeString('en-US'));
 
-    handleClick() {
-        let index = this.state.index;
-        index = (index + 1) % data.length;
-        this.setState({
-            index: index
-        })
-    }
-    render() {
-        const quote = data[this.state.index];
-        const myStyle = {
-            backgroundColor: quote.color
-        }
-        return (
-            <div className="quotesWrapper" style={myStyle}>
-                <div className="quoteBlock">
-                    <Quote text={quote.text} author={quote.author} />
-                    <button className="next-quote" style={myStyle} onClick={this.handleClick}>New Quote</button>
-                </div>
-            </div>
-        )
-    }
+    (
+        () => setInterval(() => { setTime(new Date().toLocaleTimeString('en-US')) }, 1000)
+    )();
+
+    return (
+        <div className='wrapper'>
+            <h1>The time is</h1>
+            <p className='time'>{time}</p>
+        </div>
+    )
 }
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Quotes />
-  </React.StrictMode>
+    <React.StrictMode>
+        <Clock />
+    </React.StrictMode>
 );
+
 
